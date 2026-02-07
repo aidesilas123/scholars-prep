f29fa2dfb8b34861b7fa7617d1d07e2090e79d50
Aide Silas <aidesilas123@gmail.com>
2026-02-07 11:51:17 +0100

    Final Build: Permanent bridge to Vercel with cache fix

--- android/app/src/main/java/com/scholarsprep/app/MainActivity.java
+++ android/app/src/main/java/com/scholarsprep/app/MainActivity.java
@@ -1,5 +1,20 @@
 package com.scholarsprep.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Force the WebView to always load the live site and never cache
+        WebView webView = this.bridge.getWebView();
+        WebSettings settings = webView.getSettings();
+        
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webView.clearCache(true);
+    }
+}
